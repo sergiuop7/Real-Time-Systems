f9b18ace4d76998c57ee326b65b97e32d585ce30
SergiuOprea1 <sergiuoprea13@gmail.com>
2023-03-13 12:49:23 +0200

    Examples Lab3

---
+++ src/Lab3/Example2/Consumer.java
@@ -0,0 +1,19 @@
+package Lab3.Example2;
+
+class Consumer extends Thread{
+
+    private Buffer bf;
+
+    Consumer(Buffer bf){this.bf=bf;}
+
+    public void run() {
+
+        while (true){
+
+            System.out.println("Consumer "+this.getName()+ " received >> "+bf.get());
+
+        }
+
+    }
+
+}
---
+++ src/Lab3/Example5/ReadThread.java
@@ -0,0 +1,31 @@
+package Lab3.Example5;
+
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+class ReadThread extends Thread{
+
+    private PipedInputStream pi;
+
+    ReadThread(){
+
+        pi = new PipedInputStream();
+
+    }
+
+    public void run(){
+
+        try{
+
+            while (true){
+                if (pi.available()>0) {
+                    System.out.println("Read Thread is received : "+pi.read());
+                }
+            }
+        }
+        catch(Exception e){}
+    }
+    void conect(PipedOutputStream os)throws Exception{
+        pi.connect(os);
+    }
+}
